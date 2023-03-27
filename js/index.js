f82bedbf3020eccad9f48750d656850f29cefeca
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-03-27 11:23:53 -0500

    minor fixes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 import { validate, validateTextArea } from "./validateForms.js";
 
 const logInRef = document.querySelector("[data-loginBtn]");
-const consolesRef = document.querySelector("[data-consoles]");
 
 const refs = () => {
   logInRef.addEventListener("click", () => {
     window.location.href = "/screens/login.html";
-  });
-  consolesRef.addEventListener("click", () => {
-    window.location.href = "/screens/consolas.html";
   });
 };
 
