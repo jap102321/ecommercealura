4c679e0e87b80d688c3d309994fc9037d1c05f54
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-04-02 21:15:26 -0500

    minor fix in adding product +´modal added / todo: need to add close button in modal - solve log in bug

--- js/controller/add-productController.js
+++ js/controller/add-productController.js
@@ -1,6 +1,34 @@
 import { productService } from "../services/productService.js";
 
 const addProductForm = document.querySelector("[data-submitProduct]");
+const image = document.querySelector("[data-imagePost]");
+let click_counter = 0;
+image.value = "http://127.0.0.1:5500/assets/product/";
+image.addEventListener("click", () => {
+  click_counter += 1;
+
+  if (click_counter == 1) {
+    const selectPage = document.querySelector("body");
+    const divModal = document.createElement("div");
+    divModal.classList.add("modal");
+
+    const contentModal = `
+    <div class="inner__modal">
+     <div class="modal__img>
+      <img src="../assets/icons/close-md-svgrepo-com.svg" alt=""> 
+     </div>
+     <p class="text__modal">Consultar el nombre de la imagen y 
+      su subcarpeta para añadirlo, en caso de no saber consultar con el departamento de IT</p>
+    </div>
+  `;
+    divModal.innerHTML = contentModal;
+
+    divModal.addEventListener("click", () => {
+      selectPage.removeChild(divModal);
+    });
+    selectPage.appendChild(divModal);
+  }
+});
 
 addProductForm.addEventListener("submit", (e) => {
   e.preventDefault();
